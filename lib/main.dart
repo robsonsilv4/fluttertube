ccf70d760df3c27379dcf3b06e5ed445872c8738
robsonsilv4 <robsonsilv410@gmail.com>
2019-12-28 10:32:04 -0300

    Projeto inicial

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertube/screens/home_page.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: "FlutterTube",
+      home: Home(),
+    ),
+  );
+}
